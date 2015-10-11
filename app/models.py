b786e781de49d87081d03c5463dba024474fca86
cesardeazevedo <cesardeazevedo@outlook.com>
2015-10-10 23:33:53 -0300

    Updated model

--- app/models.py
+++ app/models.py
@@ -1,13 +1,12 @@
 from django.db import models
 
-class Users(models.Model):
+class User(models.Model):
     id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=20)
-    last_name = models.CharField(max_length=30)
+    fb_id = models.CharField(max_length=150)
+    name = models.CharField(max_length=50)
     email = models.EmailField(unique=True)
-    password = models.CharField(max_length=30)
     token = models.CharField(max_length=250)
-    created_at = models.DateField()
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __unicode__(self):
         return self.name
